8b3be936322ee358018ba0259a0cd8c920be072b
WANG LEE <wanglee@WANGs-MacBook-Pro.local>
2021-03-16 17:38:45 +0900

    add: models.py on account app

---
+++ account/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Models):
+    username        = models.CharField(max_length=45)
+    password        = models.CharField(max_length=1000)
+    admin           = models.BooleanField(default=False)
+    profile_picture = models.URLField(max_length=2000, null=True)
+    created_at      = models.DateTimeField(auto_now=False, auto_now_add=True)
+    email           = models.EmailField(max_length=254, unique=True)
+
+    class Meta:
+        db_table = 'users'
