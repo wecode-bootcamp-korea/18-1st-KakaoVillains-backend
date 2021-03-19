7e0cbf8cd4d157cbe3e49cb8ae622960f417221d
Won <jungwon1423@gamil.com>
2021-03-19 10:09:06 +0900

    Modify: feed.views.py FeedindexView 수정

--- feed/urls.py
+++ feed/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from feed.views import FeedIndexView, FeedView
+from feed.views import FeedIndexView
 
 
 urlpatterns = [
     path('/feedindex', FeedIndexView.as_view()),
-    path('/feed', FeedView.as_view())
+    # path('/feed', FeedView.as_view())
 ]
--- feed/views.py
+++ feed/views.py
@@ -7,6 +7,7 @@ from django.core      import exceptions
 
 from feed.models    import Feed, FeedImage, Reply
 from account.models import User
+from product.models import Product, ProductImage
 
 
 class FeedIndexView(View):
@@ -20,80 +21,98 @@ class FeedIndexView(View):
             image_url  = [element.image_url for element in image_urls]
             user       = User.objects.get(id=feed.user_id)
             reply      = Reply.objects.filter(Q(feed_id=feed.id) & Q(parent_id=None)).order_by('-created_at').first()
+            recommend_products = Feed.objects.get(id=feed.id).products.all()
             
             if not reply:
-                reply = ''
+                reply_username = ''
+                reply_content = ''
+            else:
+                reply_username = reply.user.username
+                reply_content  = reply.content            
+
+            if not recommend_products:
+                recommend_product = []
+            else:
+                recommend_product = []
+                for product in recommend_products:
+                    product_dict = {
+                        'name'      : product.name,
+                        'price'     : product.price,
+                        'image_url' : product.productimage_set.first().image_url
+                    }
+                    recommend_product.append(product_dict)
 
             my_dict = {
-                'id'              : feed.id,
-                'username'        : user.username,
-                'title'           : feed.title,
-                'content'         : feed.content,
-                'image_url'       : image_url,
-                'profile_picture' : user.profile_picture_url,
-                'like_count'      : feed.like_count,
-                'reply_count'     : feed.reply_count,
-                'reply_username'  : reply.user.username,
-                'reply'           : reply.content
+                'id'                 : feed.id,
+                'username'           : user.username,
+                'title'              : feed.title,
+                'content'            : feed.content,
+                'image_url'          : image_url,
+                'profile_picture'    : user.profile_picture_url,
+                'like_count'         : feed.like_count,
+                'reply_count'        : feed.reply_count,
+                'reply_username'     : reply_username,
+                'reply'              : reply_content,
+                'recommend_products' : recommend_product
             }
 
             result.append(my_dict)
 
         return JsonResponse({'result' : result}, status=200)
 
-class FeedView(View):
-    def post(self, request):
-        try:
-            data        = json.loads(request.body)
-            feed        = Feed.objects.get(id=data['feed_id'])
-            feed_images = FeedImage.objects.filter(feed_id=feed.id)
-            image_url   = [element.image_url for element in feed_images]
-            replies     = feed.reply_set.all()
+# class FeedView(View):
+#     def post(self, request):
+#         try:
+#             data        = json.loads(request.body)
+#             feed        = Feed.objects.get(id=data['feed_id'])
+#             feed_images = FeedImage.objects.filter(feed_id=feed.id)
+#             image_url   = [element.image_url for element in feed_images]
+#             replies     = feed.reply_set.all()
 
-            reply_list = []
-            reply_of_reply_list = []
-            for reply in replies:
-                if not reply.parent_id:
-                    reply_dict = {
-                        'id'             : reply.id,
-                        'reply'          : reply.content,
-                        'reply_username' : reply.user.username,
-                        'like_count'     : reply.like_count,
-                    }
-                    reply_list.append(reply_dict)
+#             reply_list = []
+#             reply_of_reply_list = []
+#             for reply in replies:
+#                 if not reply.parent_id:
+#                     reply_dict = {
+#                         'id'             : reply.id,
+#                         'reply'          : reply.content,
+#                         'reply_username' : reply.user.username,
+#                         'like_count'     : reply.like_count,
+#                     }
+#                     reply_list.append(reply_dict)
                 
-                if reply.parent_id:
-                    reply_of_reply_dict = {
-                        'id'             : reply.id,
-                        'reply'          : reply.content,
-                        'reply_username' : reply.username,
-                        'like_count'     : reply.like_count,
-                        'reply_id'       : reply.reply_id
-                    }
-                    reply_of_reply_list.append(reply_of_reply_dict)
+#                 if reply.parent_id:
+#                     reply_of_reply_dict = {
+#                         'id'             : reply.id,
+#                         'reply'          : reply.content,
+#                         'reply_username' : reply.username,
+#                         'like_count'     : reply.like_count,
+#                         'reply_id'       : reply.reply_id
+#                     }
+#                     reply_of_reply_list.append(reply_of_reply_dict)
                 
 
-            result = []
+#             result = []
 
-            my_dict = {
-                'id'              : feed.id,
-                'username'        : feed.user.username,
-                'title'           : feed.title,
-                'content'         : feed.content,
-                'image_url'       : image_url,
-                'profile_picture' : feed.user.profile_picture_url,
-                'like_count'      : feed.like_count,
-                'reply_count'     : feed.reply_count,
-                'reply'           : reply_list,
-                'reply_of_reply'  : reply_of_reply_list
+#             my_dict = {
+#                 'id'              : feed.id,
+#                 'username'        : feed.user.username,
+#                 'title'           : feed.title,
+#                 'content'         : feed.content,
+#                 'image_url'       : image_url,
+#                 'profile_picture' : feed.user.profile_picture_url,
+#                 'like_count'      : feed.like_count,
+#                 'reply_count'     : feed.reply_count,
+#                 'reply'           : reply_list,
+#                 'reply_of_reply'  : reply_of_reply_list
 
-            }
-            result.append(my_dict)
+#             }
+#             result.append(my_dict)
 
-            return JsonResponse({'result' : result}, status=200)
+#             return JsonResponse({'result' : result}, status=200)
 
-        except json.JSONDecodeError:
-            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
+#         except json.JSONDecodeError:
+#             return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
 
-        except Feed.DoesNotExist:
-            return JsonResponse({'message': 'INVALID FEED_ID'}, status=400)
+#         except Feed.DoesNotExist:
+#             return JsonResponse({'message': 'INVALID FEED_ID'}, status=400)
